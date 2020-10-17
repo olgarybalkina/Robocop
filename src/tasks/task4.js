ea1304bae01c7d134d957135feed64a589668853
olgarybalkina <o.rybalkina2709@gmail.com>
2020-10-17 10:48:57 +0300

    task4

--- src/tasks/task4.js
+++ src/tasks/task4.js
@@ -1,3 +1,11 @@
 function deleteChars(str) {
-   
+   if (typeof str !== 'string') return;
+
+   const slicerString = str.slice(0, - 1);
+   return(slicerString.length);
+ 
+   return `${length.slicerString<2 ? 'Пустая строка': slicerString}`;
 }
+
+console.log = (deleteChars('Hello'));
+console.log = (deleteChars('A'));
